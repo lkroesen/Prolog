0523da5d584b159edd8a8f6c8370199cd4e582d8
Lars Stegman <l.s.stegman@student.tudelft.nl>
2015-02-23 19:46:25 +0100

    Fixed ex7

--- ex7.pl
+++ ex7.pl
@@ -1,3 +1,5 @@
+% on/2: the first block is on top of the second block.
+
 on(b, a).
 on(c, b).
 on(d, c).
@@ -6,11 +8,17 @@ on(e, d).
 above(X, Y) :- on(X, Y).
 above(X, Y) :- on(X,Z), above(Z,Y).
 
-on(X, Y, 1) :- on(X, Y).
-above(X, Y, Value, Accumulator) :- on(X, Y, Z), Value is Z + Accumulator.
-above(X, Y, Value, Accumulator) :- on(X, Z), above(Z, Y, N, Accumulator), Value is N + 1.
+% If de top block is on top of another block, the new accumulator value is increased by one and the function numberOfBlocksUnder is called on the bottom block.
+numberOfBlocksUnder(Top, A, H) :- on(Top, Y), Anew is A + 1, numberOfBlocksUnder(Y, Anew, H).
 
-atLeastThree(X) :- above(X, _, Z, 0), Z >= 3.
+% If no block can be found underneath the bottom block, the number of blocks is equal to the number of blocks in the accumulator.
+numberOfBlocksUnder(Top, A, A) :- \+ on(Top, _).
 
-exactlyThree(X) :- above(X, _, Z, 0), Z =:= 3.
-%DOESNT WORK CORRECTLY YET.
+exactlyThree(X) :- numberOfBlocksUnder(X, 0, L), L =:= 3.
+atLeastThree(X) :- numberOfBlocksUnder(X, 0, L), L >= 3.
+
+exactlyX(X, Y) :- numberOfBlocksUnder(X, 0, L), L =:= Y.
+atLeastX(X, Y) :- numberOfBlocksUnder(X, 0, L), L >= Y.
+
+
+% We're not sure how we can fix that prolog gives 2 answers when exactlyThree/1 and atLeastThree/1 are succesful the first time. When it is false, it only gives the correct answer.
