0f2e357e3ce68863ecfad49eff09a6a828754169
lkroesen <L.Kroesen@student.tudelft.nl>
2015-03-05 14:29:44 +0100

    Update and rename Assignment2 to Assignment2/ex4a2.pl

---
+++ Assignment2/ex4a2.pl
@@ -0,0 +1,7 @@
+movie('The Imitation Game',[actor('Knightely','Keira'),actor('Cumberbatch','Benedict')],
+[genre('drama')],114,13,8).
+movie('The Godfather',[actor('Brando','Marlon')],[genre('drama'),genre('crime')],170,18,9).
+movie('A Bad Movie',[actor('Eastwood','Clint')],[genre('action')],100,16,2).
+movie('Z Good Movie',[actor('Thurman','Uma')],[genre('thriller')],90,16,10).
+listMoviesByName(L):- bagof(X,getMovies(X),Li), sort(Li,L).
+getMovies(H):- movie(X,_,_,_,_,_), H = X.
